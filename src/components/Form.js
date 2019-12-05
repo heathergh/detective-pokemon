2be2b163e0355065e180b59d5112cbf88374b86c
Heather Hamilton <hghamilton+hackeryou@gmail.com>
2019-12-05 12:55:49 -0500

    Added form component

---
+++ src/components/Form.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+class Form extends Component {
+    constructor() {
+        super();
+    }
+
+    componentDidMount() {
+    
+    }
+
+    render() {
+        return (
+
+        )
+    }
+}
+
+export default Form;
