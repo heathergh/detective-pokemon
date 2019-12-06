cccbc8d85d2980ba8f9c4022a7c4661f8b5b55f2
Heather Hamilton <hghamilton+hackeryou@gmail.com>
2019-12-06 15:12:00 -0500

    Added error handling to form submission

--- src/components/ErrorMessage.js
+++ src/components/ErrorMessage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 const ErrorMessage = props => {
     return (
-        <div role="alert" className="error">
+        <div role="alert" className="error" id={props.form_error_message}>
             <p>{props.children}</p>
         </div>
     )
--- src/components/Select.js
+++ src/components/Select.js
@@ -1,23 +1,38 @@
 import React from 'react';
 
-
 // get object from parent
 // get out two dynamic properties: paramName, clientSideName)
-const Select = ({arrayProp, optionValue, optionName, labelFor, selectLabel, selectName, changeHandler}) => {
+const Select = ({arrayProp, optionValue, optionName, labelFor, label, selectName, changeHandler, isValid}) => {
     const elements = arrayProp.map((element, index) => {
         return (
             <option key={index} value={element[optionValue]}>{element[optionName]}</option>
         )
     })
 
+    let errorMessageId = '';
+    let ariaValidator = false;
+
+    if (!isValid) {
+        errorMessageId = 'error-description';
+        ariaValidator = true;
+    } else {
+        errorMessageId = '';
+        ariaValidator = false;
+    }
+
     return (
         <>
-            <label htmlFor={labelFor}>{selectLabel}</label>
+            <label htmlFor={labelFor}>{label}</label>
 
             <select 
                 name={selectName}
                 id={labelFor}
                 onChange={changeHandler}
+                required={true}
+                aria-required="true"
+                aria-label={`select one of the ${label}`}
+                aria-invalid={ariaValidator}
+                aria-describedby={errorMessageId}
             >
                 <option value="">--Please choose an option--</option>
                 {elements}
