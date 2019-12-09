f9e1662bd0c3eff63ee3a0b5876ab0927d3aef88
elenaserova <elein.serova@gmail.com>
2019-12-09 17:10:38 -0500

    results page styles updated

--- src/components/GameResult.js
+++ src/components/GameResult.js
@@ -7,27 +7,27 @@ const GameResult = ({
   reasonForSuccess = 'Fire pokemon can handle fires.',
   playAgain
 }) => (
-  <div>
-    <h2>
-      {resultOfGame
-        ? 'Great job, you solved the crime!'
-        : 'Better luck next time!'}
-    </h2>
     <div>
-      { 
-        resultOfGame
-          ? <p>
+      <h2 className="headerForResult">
+        {resultOfGame
+          ? 'Great job, you solved the crime'
+          : 'Better luck next time'}
+      </h2>
+      <div>
+        {
+          resultOfGame
+            ? <p>
               {pokemonName} solved the crime of {crimeSolved}!
             </p>
-          : <p>
+            : <p>
               The criminal got away!
             </p>
-      }
-      
-      <p>{reasonForSuccess}</p>
+        }
+
+        <p>{reasonForSuccess}</p>
+      </div>
+      <button className="playAgain" onClick={playAgain}>Solve another crime</button>
     </div>
-    <button onClick={playAgain}>Solve another crime</button>
-  </div>
-);
+  );
 
 export default GameResult;
