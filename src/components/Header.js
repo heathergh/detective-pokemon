2e8015ea2720b13e595b40b0356ba70148abe498
Heather Hamilton <hghamilton+hackeryou@gmail.com>
2020-06-26 17:44:35 -0400

    Fixed typo for state property name, shortened instructions, changed padding to get rid of vertical scroll on desktop

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,7 @@ class Header extends Component {
     super();
     this.state = {
       userName: '',
-      nameChoosen: false,
+      nameChosen: false,
       errorMessage: ''
     }
   }
@@ -20,11 +20,14 @@ class Header extends Component {
 
   handleNameSubmit = (e) => {
     e.preventDefault();
-    if (this.state.userName !== '') {
+    const trimmedUserName = this.state.userName.trim();
+
+    if (trimmedUserName) {
       this.setState({
-        nameChoosen: true
+        nameChosen: true
+      }, () => {
+        this.props.nameSubmit();
       });
-      this.props.nameSubmit();
     } else {
       this.setState({
         errorMessage: 'Error: please enter your name.',
@@ -35,26 +38,26 @@ class Header extends Component {
   render() {
     return (
       <header className="wrapper">
-        <h1 tabIndex='0'>Detective Pokemon</h1>
+        <h1>Detective Pokemon</h1>
 
         {
-          !this.state.nameChoosen ?
-            <div>
-              <p>It is dark times out there. In this new world, Pokemon come to the rescue and support the UK law enforcement. As a detective you will help support the UK police officers with your Pokemon partner! </p>
-              <p>Pick a location and a crime, and then decide which Pokemon partner you'll bring with you to solve the crime.</p>
-              <p>Each type of crime requires a different type of Pokemon. What crime will you help solve today?</p>
+          !this.state.nameChosen
+        ?
+          <>
+            <p>It's a dark new world out there. As a detective with UK Law Enforcement, you will help solve crimes in the UK with a Pokemon partner of your choosing! </p>
+            <p>Each type of crime requires a different type of Pokemon. What crime will you help solve today?</p>
 
-              <form onSubmit={this.handleNameSubmit}>
+            <form onSubmit={this.handleNameSubmit}>
 
-                <label htmlFor="userName">Your name:</label>
-                <input id="userName" type="text" name="userName" value={this.state.userName} onChange={this.handleInputChange} />
+              <label htmlFor="userName">Your name:</label>
+              <input id="userName" type="text" name="userName" value={this.state.userName} onChange={this.handleInputChange} onBlur={this.handleInputChange}/>
 
-                <button>Submit</button>
-                {this.state.errorMessage !== '' ? <ErrorMessage id={'error-description'}>{this.state.errorMessage}</ErrorMessage> : null}
-              </form>
-            </div>
-            :
-            <h2>Detective {this.state.userName} is on duty</h2>
+              <button>Submit</button>
+              {this.state.errorMessage !== '' ? <ErrorMessage id={'error-description'}>{this.state.errorMessage}</ErrorMessage> : null}
+            </form>
+          </>
+        :
+          <h2>Detective {this.state.userName} is on duty</h2>
         }
 
       </header>
